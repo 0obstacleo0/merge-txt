319555d40f7e195fb287658b5781b8a8ca01457e
0obstacleo0 <0obstacleo0@gmail.com>
2024-06-23 11:24:15 +0900

    Read関数のテストコード追加

---
+++ file/file_test.go
@@ -0,0 +1,47 @@
+package file
+
+import (
+	"os"
+	"testing"
+)
+
+func TestRead(t *testing.T) {
+	tempDir, err := os.MkdirTemp("/tmp", "testdir")
+	if err != nil {
+		t.Fatalf("Failed to create temporary directory:%v", err)
+	}
+	defer os.Remove(tempDir)
+
+	tempFile1, err := os.CreateTemp(tempDir, "testfile")
+	if err != nil {
+		t.Fatalf("Failed to create temporary file: %v", err)
+	}
+	defer os.Remove(tempFile1.Name())
+
+	content1 := "name,age\ntaro,12\nrika,3"
+	if _, err := tempFile1.WriteString(content1); err != nil {
+		t.Fatalf("Failed to write to temporary file: %v", err)
+	}
+	tempFile1.Close()
+
+	tempFile2, err := os.CreateTemp(tempDir, "testfile")
+	if err != nil {
+		t.Fatalf("Failed to create temporary file: %v", err)
+	}
+	defer os.Remove(tempFile2.Name())
+
+	content2 := "name,age\nmayumi,22\nmiyu,33\nkenji,11"
+	if _, err := tempFile2.WriteString(content2); err != nil {
+		t.Fatalf("Failed to write to temporary file: %v", err)
+	}
+	tempFile2.Close()
+
+	contents, err := Read(tempDir, 1)
+	if err != nil {
+		t.Errorf("Read returned an error: %v", err)
+	}
+
+	if len(contents) != 6 {
+		t.Errorf("Expected rows 6, but got %d", len(contents))
+	}
+}
